f977244d3e5be0ee5bc31baad7a39a55dbb6b57b
AssoRBE <association.rbe@gmail.com>
2025-10-22 22:38:56 +0200

    mise à jour

--- src/finance.js
+++ src/finance.js
@@ -6,16 +6,8 @@ router.get('/balance', (req, res) => {
   res.json({ amount: 0, currency: 'EUR', updatedAt: new Date().toISOString() });
 });
 
-router.get('/balance/history', (req, res) => {
-  res.json([]);
-});
-
-router.get('/scheduled-operations', (req, res) => {
-  res.json([]);
-});
-
-router.get('/transactions', (req, res) => {
-  res.json([]);
-});
+router.get('/balance/history', (req, res) => res.json([]));
+router.get('/scheduled-operations', (req, res) => res.json([]));
+router.get('/transactions', (req, res) => res.json([]));
 
 module.exports = router;
